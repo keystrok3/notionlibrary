1012eff8ba1e189caf45fe5a12c1376dfdce8b35
Josiah Mukhwana <jwalutsachi4@gmail.com>
2023-11-25 16:28:52 +0300

    Added Books model ./models/books.js

---
+++ models/books.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+const { sequelize } = require('../config/db');
+
+class Books extends Model {
+
+    static associate(models) {
+        //associations
+    }
+}
+
+
+Books.init({
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    isbn: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+        primaryKey: true
+    },
+
+    author: {
+        type: DataTypes.STRING,
+    },
+
+    number_available: {
+        type: DataTypes.INTEGER
+    }
+}, {
+    
+    sequelize,
+    tableName: 'books',
+    modelName: 'Books'   
+});
+
+
+// Create table
+Books
+.sync()
+.then(() => console.log("Table Created"))
+.catch(console.log);
+
+
+module.exports = Books;
